f4e627fc3f49b17ec563ec1b18bc67da85809d1c
Michael Dudkowiak <michael.dudkowiak@tum.de>
2023-06-11 22:55:30 +0200

    Add spline code, utils and beginnings of docs

--- docs/src/index.md
+++ docs/src/index.md
@@ -1,1 +1,18 @@
 # MonotonicSplines.jl
+
+A package implementing monotonous spline function for applications in spline-based approaches to Normalizing FLows [(Wikipedia)](https://en.wikipedia.org/wiki/Flow-based_generative_model).
+
+This package currently includes the *monotonous rational quadratic splines* as defined in ["Neural Spline Flows, Durkan et al. 2019"](https://arxiv.org/abs/1906.04032).
+
+## Package Features
+---
+
+This package was created with high performance as the overall goal and features automatic code differentiation, powered by [Zygote.jl](https://fluxml.ai/Zygote.jl/latest/), and GPU compatibility, powered by [KernelAbstractions.jl](https://juliagpu.github.io/KernelAbstractions.jl/stable/).
+
+## Manual Outline
+---
+
+```@contents
+Pages = ["introduction.md","guide.md"]
+Depth = 3
+```
---
+++ src/utils.jl
@@ -0,0 +1,123 @@
+# This file is a part of EuclidianNormalizingFlows.jl, licensed under the MIT License (MIT)
+
+function get_params(θ_raw::AbstractArray, n_dims_trafo::Integer, B::Real = 5.)
+
+    N = size(θ_raw, 2)
+    K = Int((size(θ_raw,1)/n_dims_trafo+1)/3)
+    θ = reshape(θ_raw, :, n_dims_trafo, N)
+
+    device = KernelAbstractions.get_device(θ_raw)
+
+    w = device isa GPU ? cat(cu(repeat([-B], 1, n_dims_trafo, N)), _cumsum_tri(_softmax_tri(θ[1:K,:,:])); dims = 1) : cat(repeat([-B], 1, n_dims_trafo, N), _cumsum_tri(_softmax_tri(θ[1:K,:,:])), dims = 1)
+    h = device isa GPU ? cat(cu(repeat([-B], 1, n_dims_trafo, N)), _cumsum_tri(_softmax_tri(θ[K+1:2K,:,:])); dims = 1) : cat(repeat([-B], 1, n_dims_trafo, N), _cumsum_tri(_softmax_tri(θ[K+1:2K,:,:])), dims = 1)
+    d = device isa GPU ? cat(cu(repeat([1], 1, n_dims_trafo, N)), _softplus_tri(θ[2K+1:end,:,:]), cu(repeat([1], 1, n_dims_trafo, N)); dims = 1) : cat(repeat([1], 1, n_dims_trafo, N), _softplus_tri(θ[2K+1:end,:,:]), repeat([1], 1, n_dims_trafo, N), dims = 1)
+
+    return w, h, d
+end
+
+export get_params
+
+# just a hack, pls dont judge :(
+function _sort_dimensions(y₁::AbstractArray, y₂::AbstractArray, mask::AbstractVector)
+    
+    if mask[1]
+        res = reshape(y₁[1,:],1,size(y₁,2))
+        c=2
+    else
+        res = reshape(y₂[1,:],1,size(y₁,2))
+        c=1
+    end
+
+    for (i,b) in enumerate(mask[2:end])
+        if b
+            res = vcat(res, reshape(y₁[c,:],1,size(y₁,2)))
+            c+=1
+        else
+            res = vcat(res, reshape(y₂[i+1,:],1,size(y₂,2)))
+        end
+    end
+
+    return res
+end
+
+function _softmax(x::AbstractVector)
+
+    exp_x = exp.(x)
+    sum_exp_x = sum(exp_x)
+
+    return exp_x ./ sum_exp_x 
+end
+
+function _softmax(x::AbstractMatrix)
+
+    val = cat([_softmax(i) for i in eachrow(x)]..., dims=2)'
+
+    return val 
+end
+
+function _softmax_tri(x::AbstractArray)
+    exp_x = exp.(x)
+    inv_sum_exp_x = inv.(sum(exp_x, dims = 1))
+
+    return inv_sum_exp_x .* exp_x
+end
+
+export _softmax_tri
+
+function _cumsum(x::AbstractVector; B = 5)
+    return 2 .* B .* cumsum(x) .- B 
+end
+
+function _cumsum(x::AbstractMatrix)
+
+    return cat([_cumsum(i) for i in eachrow(x)]..., dims=2)'
+end
+
+function _cumsum_tri(x::AbstractArray, B::Real = 5.)
+    
+    return 2 .* B .* cumsum(x, dims = 1) .- B 
+end
+
+export _cumsum_tri
+
+function _softplus(x::AbstractVector)
+
+    return log.(exp.(x) .+ 1) 
+end
+
+function _softplus(x::AbstractMatrix)
+
+    val = cat([_softplus(i) for i in eachrow(x)]..., dims=2)'
+
+    return val
+end
+
+function _softplus_tri(x::AbstractArray)
+    return log.(exp.(x) .+ 1) 
+end
+
+export _softplus_tri
+
+midpoint(lo::T, hi::T) where T<:Integer = lo + ((hi - lo) >>> 0x01)
+binary_log(x::T) where {T<:Integer} = 8 * sizeof(T) - leading_zeros(x - 1)
+
+function searchsortedfirst_impl(
+        v::AbstractVector, 
+        x::Real
+    )
+    
+    u = one(Integer)
+    lo = one(Integer) - u
+    hi = length(v) + u
+    
+    n = binary_log(length(v))+1
+    m = one(Integer)
+    
+    @inbounds for i in 1:n
+        m_1 = midpoint(lo, hi)
+        m = Base.ifelse(lo < hi - u, m_1, m)
+        lo = Base.ifelse(v[m] < x, m, lo)
+        hi = Base.ifelse(v[m] < x, hi, m)
+    end
+    return hi
+end
