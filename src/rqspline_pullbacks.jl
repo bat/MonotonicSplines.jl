f1b90b6148a55f5d998b48ece54d61963acc4901
Michael Dudkowiak <michael.dudkowiak@tum.de>
2023-12-13 15:22:16 +0100

    Correct backward rqs gradient calculation

--- src/rqspline_pullbacks.jl
+++ src/rqspline_pullbacks.jl
@@ -3,30 +3,30 @@
 """
     rqs_pullback(param_eval_function::Function, x::AbstractArray{<:Real}, w::AbstractArray{<:Real}, h::AbstractArray{<:Real}, d::AbstractArray{<:Real}, tangent_1::AbstractArray, tangent_2::AbstractArray)
 
-Compute and return the gradients of the rational quadratic spline functions characterized by `w`, `h`, and `d`, evaluated at the values in `x` with respect to `w`, `h`, and `d`. 
+Compute the gradients of the rational quadratic spline functions characterized by `w`, `h`, and `d`, evaluated at the values in `x` with respect to `w`, `h`, and `d`. 
 
 This function is designed to make the transformation using Rational Quadratic Splines in this package automatically differentiable.
 Whether the gradients of the forward or backward spline functions are calculated is determined by the `param_eval_function` argument.
 
 # Arguments
-- `param_eval_function`: The function used to evaluate a spline segment. Different functions are used for the forward and backward passes.
+- `param_eval_function`: The function used to evaluate a spline segment. Different functions are used for the forward and backward spline functions.
 - `x`: An array of real numbers at which the spline functions are evaluated.
 - `w`, `h`, `d`: Arrays that hold the width, height, and derivative parameters of the spline functions, respectively.
 - `tangent_1`, `tangent_2`: Arrays that hold the tangent vectors for the transformed output and the log abs det jacobians respectively.
 
 # Returns
 Three values are returned:
-- `∂y∂w + ∂LogJac∂w`: The gradient of the spline functions with respect to `w`.
-- `∂y∂h + ∂LogJac∂h`: The gradient of the spline functions with respect to `h`.
-- `∂y∂d + ∂LogJac∂d`: The gradient of the spline functions with respect to `d`.
-
-# Usage
-The function is used to compute the gradients of the spline functions, making the transformation using Rational Quadratic Splines in this package automatically differentiable.
-
-# Performance
-The function leverages parallel computing for high performance. It uses `KernelAbstractions.jl` for parallel execution on either a CPU or a GPU.
+- `∂y∂w + ∂LogJac∂w`: An array with the same shape as `w`, with the `[:,j,k]`-th element holding the gradient of the `[j,k]`-th element of `y` with respect to the width parameters
+                      plus the gradient of the logarithm of the absolute value of the derivative of this `[j,k]`-th element of `y` with respect to the `[j,k]`-th element of `x`, with respect to the width parameters. 
+                      For Example, the `[i,j,k]` element of this array is `∂yⱼₖ/∂wᵢⱼₖ + ∂(log(abs(∂yⱼₖ/∂xⱼₖ)))/∂wᵢⱼₖ`.
+- `∂y∂h + ∂LogJac∂h`: An array with the same shape as `h`, holding the same gradients as described above, but with respect to the height parameters.
+- `∂y∂d + ∂LogJac∂d`: An array with the same shape as `d`, holding the same gradients as described above, but with respect to the derivative parameters.
 
 # Note
+Since only one segment of each spline is evaluated for one element of `x`, the returned gradients are filled with zeros, except for the parameters of the segment that is evaluated for that element of `x`.
+For example, if the `[j,k]` -th element of `x` falls in the `l`-th bin of the interval mask, the `[:,j,k]` entries in `∂y∂w + ∂LogJac∂w` are all zero, except the `[l,j,k]` and `[l+1,j,k]` elements, which hold 
+the (generally non-zero) values `∂yⱼₖ/∂wₗⱼₖ + ∂(log(abs(∂yⱼₖ/∂xⱼₖ)))/∂wₗⱼₖ` and `∂yⱼₖ/∂wₗ₊₁ⱼₖ + ∂(log(abs(∂yⱼₖ/∂xⱼₖ)))/∂wₗ₊₁ⱼₖ` respectively.
+
 The function executes in a kernel, on the same backend as `x` is stored (CPU or GPU), and the output is also returned on the same backend.
 """
 function rqs_pullback(
@@ -98,9 +98,11 @@ This kernel function calculates the gradients of the rational quadratic spline f
 - `w`, `h`, `d`: Arrays that hold the width, height, and derivative parameters of the spline functions, respectively.
 - `y`: An array where the transformed values are stored.
 - `logJac`: An array where the sums of the values of the logarithm of the absolute values of the determinant of the Jacobians of the spline functions applied to a column of `x` are stored.
-- `∂y∂w_tangent`, `∂y∂h_tangent`, `∂y∂d_tangent`: Arrays where the gradients of the spline functions with respect to `w`, `h`, and `d` are stored, respectively.
-- `∂LogJac∂w_tangent`, `∂LogJac∂h_tangent`, `∂LogJac∂d_tangent`: Arrays where the gradients of `logJac` with respect to `w`, `h`, and `d` are stored, respectively.
+- `∂y∂w_tangent`, `∂y∂h_tangent`, `∂y∂d_tangent`: Arrays that will contain the gradients of the spline functions with respect to `w`, `h`, and `d`, respectively.
+- `∂LogJac∂w_tangent`, `∂LogJac∂h_tangent`, `∂LogJac∂d_tangent`: Arrays that will contain the gradients of `logJac` with respect to `w`, `h`, and `d`, respectively.
 - `tangent_1`, `tangent_2`: Arrays that hold the tangent vectors for the forward pass.
+
+For an explanation of the shape and contents of the gradient arrays, see the documentation of the [`rqs_pullback()`](@ref) function.
 
 # Description
 The transformed values are stored in `y` and the sums of the values of the logarithm of the absolute values of the determinant of the Jacobians of the spline functions applied to a column of `x` are stored in `logJac`. 
@@ -181,8 +183,8 @@ end
 """
     eval_forward_rqs_params_with_grad(wₖ::M0, wₖ₊₁::M0, hₖ::M1, hₖ₊₁::M1, dₖ::M2, dₖ₊₁::M2, x::M3) where {M0<:Real,M1<:Real, M2<:Real, M3<:Real}
 
-Apply a rational quadratic spline segment to `x`, calculate the logarithm of the absolute value of the derivative of the segment at `x`, 
-and compute the gradient of that segment with respect to the spline parameters.
+Apply a rational quadratic spline segment to `x`, calculate the logarithm of the absolute value of the derivative ("LogJac") of the segment at `x`, 
+and compute the gradient of that segment and the LogJac with respect to the spline parameters.
 
 # Arguments
 - `wₖ`, `wₖ₊₁`: The width parameters of the spline segment at the edges of the `k`-th interval.
@@ -193,8 +195,8 @@ and compute the gradient of that segment with respect to the spline parameters.
 # Returns
 - `y`: The transformed value after applying the rational quadratic spline segment to `x`.
 - `logJac`: The logarithm of the absolute value of the derivative of the segment at `x`.
-- `∂y∂w`, `∂y∂h`, `∂y∂d`: The gradients of `y` with respect to the width, height, and derivative parameters, respectively.
-- `∂LogJac∂w`, `∂LogJac∂h`, `∂LogJac∂d`: The gradients of `logJac` with respect to the width, height, and derivative parameters, respectively.
+- `∂y∂w`, `∂y∂h`, `∂y∂d`: The gradients of `y` with respect to the two width, height, and derivative parameters, respectively.
+- `∂LogJac∂w`, `∂LogJac∂h`, `∂LogJac∂d`: The gradients of `logJac` with respect to the two width, height, and derivative parameters, respectively.
 """
 function eval_forward_rqs_params_with_grad(
     wₖ::M0, wₖ₊₁::M0, 
@@ -280,11 +282,27 @@ function ChainRulesCore.rrule(
     return (y, logJac), pullback
 end
 
+"""
+    eval_backward_rqs_params_with_grad(wₖ::M0, wₖ₊₁::M0, 
+                                       hₖ::M1, hₖ₊₁::M1, 
+                                       dₖ::M2, dₖ₊₁::M2, 
+                                       x::M3            ) where {M0<:Real,M1<:Real, M2<:Real, M3<:Real}
 
+Apply an inverse rational quadratic spline segment to `x`, calculate the logarithm of the absolute value of the derivative ("LogJac") of the segment at `x`, 
+and compute the gradient of that segment and the LogJac with respect to the spline parameters.
 
+# Arguments
+- `wₖ`, `wₖ₊₁`: The width parameters of the spline segment at the edges of the `k`-th interval.
+- `hₖ`, `hₖ₊₁`: The height parameters of the spline segment.
+- `dₖ`, `dₖ₊₁`: The derivative parameters of the spline segment.
+- `x`: The value at which the spline function is evaluated.
 
-
-
+# Returns
+- `y`: The transformed value after applying the inverse rational quadratic spline segment to `x`.
+- `logJac`: The logarithm of the absolute value of the derivative of the segment at `x`.
+- `∂y∂w`, `∂y∂h`, `∂y∂d`: The gradients of `y` with respect to the two width, height, and derivative parameters, respectively.
+- `∂LogJac∂w`, `∂LogJac∂h`, `∂LogJac∂d`: The gradients of `LogJac` with respect to the two width, height, and derivative parameters, respectively.
+"""
 function eval_backward_rqs_params_with_grad(
     wₖ::M0, wₖ₊₁::M0, 
     hₖ::M1, hₖ₊₁::M1, 
@@ -298,97 +316,84 @@ function eval_backward_rqs_params_with_grad(
 
     κ = Δy * (sk - dₖ) + Δy2 * (dₖ₊₁ + dₖ - 2*sk)
     β = Δy * dₖ - Δy2 * (dₖ₊₁ + dₖ - 2*sk)
-    ζ = - sk * Δy2
+    ζ = -sk * Δy2
     θ = sqrt(β*β - 4*κ*ζ)
-
-    # TODO: Simplify expressions, remove zero value expressions
 
     # Partial derivatives with respect to x
     ∂κ∂x = dₖ₊₁ + dₖ - 2*sk
-    ∂β∂x = - ∂κ∂x
-    ∂ζ∂x = - sk
-    ∂θ∂x = (1/(2*θ)) * (2 * β * ∂β∂x - 4 * (∂κ∂x * ζ + κ * ∂ζ∂x)) 
+    ∂β∂x = -∂κ∂x
+    ∂ζ∂x = -sk
+    ∂θ∂x = (1/θ) * (β * ∂β∂x - 2 * (∂κ∂x * ζ + κ * ∂ζ∂x))
 
     # Enumerator of -∂y∂x
-    μ = 2 * Δx * (∂ζ∂x * (β + θ) - ζ(∂β∂x + ∂θ∂x))
+    μ = 2 * Δx * (∂ζ∂x * (β + θ) - ζ * (∂β∂x + ∂θ∂x))
 
     # Partial derivatives with regard to wₖ, wₖ₊₁, hₖ, hₖ₊₁, dₖ, dₖ₊₁
-    ∂κ∂wₖ   = (-2Δy*Δy2) / (Δx^2) + (Δy^2) / (Δx^2)
-    ∂κ∂wₖ₊₁ = (2Δy*Δy2) / (Δx^2) + (-(Δy^2)) / (Δx^2)
-    ∂κ∂hₖ   = -dₖ₊₁ + (2Δy) / Δx + (2Δy2) / Δx + (2(hₖ - hₖ₊₁)) / Δx
-    ∂κ∂hₖ₊₁ = -dₖ + (2Δy) / Δx + (-2Δy2) / Δx
-    ∂κ∂dₖ   = -hₖ₊₁ + x
+    ∂κ∂wₖ   = (Δy^2 - 2Δy*Δy2) / Δx^2
+    ∂κ∂hₖ   = 2 * Δy2 / Δx - dₖ₊₁
+    ∂κ∂hₖ₊₁ = 2 * (Δy - Δy2) / Δx - dₖ
+    ∂κ∂dₖ   = x - hₖ₊₁
     ∂κ∂dₖ₊₁ = Δy2
-
-    ∂β∂wₖ   = (-2(hₖ - x)*Δy) / (Δx^2)
-    ∂β∂wₖ₊₁ = (-2(-hₖ + x)*Δy) / (Δx^2)
-    ∂β∂hₖ   = dₖ₊₁ + (-2Δy) / Δx + (2(hₖ - x)) / Δx
-    ∂β∂hₖ₊₁ = dₖ + (-2(hₖ - x)) / Δx
+    
+    ∂β∂wₖ   = 2 * Δy2 * Δy / Δx^2
+    ∂β∂hₖ   = dₖ₊₁ - 2 * (Δy + Δy2) / Δx
+    ∂β∂hₖ₊₁ = dₖ + 2 * Δy2 / Δx
     ∂β∂dₖ   = hₖ₊₁ - x
-    ∂β∂dₖ₊₁ = hₖ - x
+    ∂β∂dₖ₊₁ = -Δy2
 
-    ∂ζ∂wₖ   = ((hₖ - x)*Δy) / (Δx^2)
-    ∂ζ∂wₖ₊₁ = ((-hₖ + x)*Δy) / (Δx^2)
-    ∂ζ∂hₖ   = Δy / Δx + Δy2 / Δx
-    ∂ζ∂hₖ₊₁ = (hₖ - x) / Δx
+    ∂ζ∂wₖ   = -Δy2 * Δy / Δx^2
+    ∂ζ∂hₖ   = (Δy2 + Δy) / Δx
+    ∂ζ∂hₖ₊₁ = -Δy2 / Δx
     ∂ζ∂dₖ   = 0
     ∂ζ∂dₖ₊₁ = 0
 
-    ∂θ∂wₖ   = (1/(2*θ)) * (2*β * ∂β∂wₖ   - 4*(∂κ∂wₖ   + κ * ∂ζ∂wₖ  )) 
-    ∂θ∂wₖ₊₁ = (1/(2*θ)) * (2*β * ∂β∂wₖ₊₁ - 4*(∂κ∂wₖ₊₁ + κ * ∂ζ∂wₖ₊₁))
-    ∂θ∂hₖ   = (1/(2*θ)) * (2*β * ∂β∂hₖ   - 4*(∂κ∂hₖ   + κ * ∂ζ∂hₖ  ))
-    ∂θ∂hₖ₊₁ = (1/(2*θ)) * (2*β * ∂β∂hₖ₊₁ - 4*(∂κ∂hₖ₊₁ + κ * ∂ζ∂hₖ₊₁))
-    ∂θ∂dₖ   = (1/(2*θ)) * (2*β * ∂β∂dₖ   - 4* ∂κ∂dₖ  )
-    ∂θ∂dₖ₊₁ = (1/(2*θ)) * (2*β * ∂β∂dₖ₊₁ - 4* ∂κ∂dₖ₊₁)
+    ∂θ∂wₖ   = (1/θ) * (β * ∂β∂wₖ   - 2 * (∂κ∂wₖ   * ζ + κ * ∂ζ∂wₖ  ))
+    ∂θ∂hₖ   = (1/θ) * (β * ∂β∂hₖ   - 2 * (∂κ∂hₖ   * ζ + κ * ∂ζ∂hₖ  ))
+    ∂θ∂hₖ₊₁ = (1/θ) * (β * ∂β∂hₖ₊₁ - 2 * (∂κ∂hₖ₊₁ * ζ + κ * ∂ζ∂hₖ₊₁))
+    ∂θ∂dₖ   = (1/θ) * (β * ∂β∂dₖ   - 2 *  ∂κ∂dₖ   * ζ)
+    ∂θ∂dₖ₊₁ = (1/θ) * (β * ∂β∂dₖ₊₁ - 2 *  ∂κ∂dₖ₊₁ * ζ)
 
-    ∂β∂x∂wₖ =   -2 Δy / Δx^2
-    ∂β∂x∂wₖ₊₁ =  2 Δy / Δx^2
-    ∂β∂x∂hₖ =   -2 / Δx
-    ∂β∂x∂hₖ₊₁ =  2 / Δx
-    ∂β∂x∂dₖ =   -1
-    ∂β∂x∂dₖ₊₁ = -1 
-    
-    ∂ζ∂x∂wₖ =   -Δy / Δx^2
-    ∂ζ∂x∂wₖ₊₁ =  Δy / Δx^2
-    ∂ζ∂x∂hₖ =    1 / Δx
-    ∂ζ∂x∂hₖ₊₁ = -1 / Δx
-    ∂ζ∂x∂dₖ =    0
-    ∂ζ∂x∂dₖ₊₁ =  0
+    ∂κ∂x∂wₖ   = -2 * Δy / Δx^2
+    ∂κ∂x∂hₖ   =  2 / Δx
+    ∂κ∂x∂hₖ₊₁ = -∂κ∂x∂hₖ
 
-    ∂θ∂x∂wₖ =   (1 / θ) * (((∂β∂wₖ   * ∂β∂x + β * ∂β∂x∂wₖ)   - 2 * (∂κ∂x∂wₖ   * ζ + ∂κ∂x * ∂ζ∂wₖ   + ∂κ∂wₖ   * ∂ζ∂x + κ * ∂ζ∂x∂wₖ))   - ∂θ∂wₖ * ∂θ∂x)
-    ∂θ∂x∂wₖ₊₁ = (1 / θ) * (((∂β∂wₖ₊₁ * ∂β∂x + β * ∂β∂x∂wₖ₊₁) - 2 * (∂κ∂x∂wₖ₊₁ * ζ + ∂κ∂x * ∂ζ∂wₖ₊₁ + ∂κ∂wₖ₊₁ * ∂ζ∂x + κ * ∂ζ∂x∂wₖ₊₁)) - ∂θ∂wₖ₊₁ * ∂θ∂x)
-    ∂θ∂x∂hₖ =   (1 / θ) * (((∂β∂hₖ   * ∂β∂x + β * ∂β∂x∂hₖ)   - 2 * (∂κ∂x∂hₖ   * ζ + ∂κ∂x * ∂ζ∂hₖ   + ∂κ∂hₖ   * ∂ζ∂x + κ * ∂ζ∂x∂hₖ))   - ∂θ∂hₖ * ∂θ∂x)
+    ∂β∂x∂wₖ   = 2 * Δy / Δx^2
+    ∂β∂x∂hₖ   = -2 / Δx
+    ∂β∂x∂hₖ₊₁ =  -∂β∂x∂hₖ
+
+    ∂ζ∂x∂wₖ   = -Δy / Δx^2
+    ∂ζ∂x∂hₖ   =  1 / Δx
+    ∂ζ∂x∂hₖ₊₁ = -∂ζ∂x∂hₖ
+
+    ∂θ∂x∂wₖ   = (1 / θ) * (((∂β∂wₖ   * ∂β∂x + β * ∂β∂x∂wₖ  ) - 2 * (∂κ∂x∂wₖ   * ζ + ∂κ∂x * ∂ζ∂wₖ   + ∂κ∂wₖ   * ∂ζ∂x + κ * ∂ζ∂x∂wₖ  )) - ∂θ∂wₖ   * ∂θ∂x)
+    ∂θ∂x∂hₖ   = (1 / θ) * (((∂β∂hₖ   * ∂β∂x + β * ∂β∂x∂hₖ  ) - 2 * (∂κ∂x∂hₖ   * ζ + ∂κ∂x * ∂ζ∂hₖ   + ∂κ∂hₖ   * ∂ζ∂x + κ * ∂ζ∂x∂hₖ  )) - ∂θ∂hₖ   * ∂θ∂x)
     ∂θ∂x∂hₖ₊₁ = (1 / θ) * (((∂β∂hₖ₊₁ * ∂β∂x + β * ∂β∂x∂hₖ₊₁) - 2 * (∂κ∂x∂hₖ₊₁ * ζ + ∂κ∂x * ∂ζ∂hₖ₊₁ + ∂κ∂hₖ₊₁ * ∂ζ∂x + κ * ∂ζ∂x∂hₖ₊₁)) - ∂θ∂hₖ₊₁ * ∂θ∂x)
-    ∂θ∂x∂dₖ =   (1 / θ) * (((∂β∂dₖ   * ∂β∂x + β * ∂β∂x∂dₖ)   - 2 * (∂κ∂x∂dₖ   * ζ + ∂κ∂x * ∂ζ∂dₖ   + ∂κ∂dₖ   * ∂ζ∂x + κ * ∂ζ∂x∂dₖ))   - ∂θ∂dₖ * ∂θ∂x)
-    ∂θ∂x∂dₖ₊₁ = (1 / θ) * (((∂β∂dₖ₊₁ * ∂β∂x + β * ∂β∂x∂dₖ₊₁) - 2 * (∂κ∂x∂dₖ₊₁ * ζ + ∂κ∂x * ∂ζ∂dₖ₊₁ + ∂κ∂dₖ₊₁ * ∂ζ∂x + κ * ∂ζ∂x∂dₖ₊₁)) - ∂θ∂dₖ₊₁ * ∂θ∂x)
+    ∂θ∂x∂dₖ   = (1 / θ) * (((∂β∂dₖ   * ∂β∂x - β) - 2 * (ζ + ∂κ∂x * ∂ζ∂dₖ   + ∂κ∂dₖ   * ∂ζ∂x)) - ∂θ∂dₖ   * ∂θ∂x)
+    ∂θ∂x∂dₖ₊₁ = (1 / θ) * (((∂β∂dₖ₊₁ * ∂β∂x - β) - 2 * (ζ + ∂κ∂x * ∂ζ∂dₖ₊₁ + ∂κ∂dₖ₊₁ * ∂ζ∂x)) - ∂θ∂dₖ₊₁ * ∂θ∂x)
 
-    
-    ∂μ∂wₖ   = 2 * (Δx * (∂ζ∂x∂wₖ   * (β + θ) + ∂ζ∂x * (∂β∂wₖ   + ∂θ∂wₖ)   - ∂ζ∂wₖ   * (∂β∂x + ∂θ∂x) - ζ * (∂β∂x∂wₖ   + ∂θ∂x∂wₖ))   - μ / (2*Δx))
-    ∂μ∂wₖ₊₁ = 2 * (Δx * (∂ζ∂x∂wₖ₊₁ * (β + θ) + ∂ζ∂x * (∂β∂wₖ₊₁ + ∂θ∂wₖ₊₁) - ∂ζ∂wₖ₊₁ * (∂β∂x + ∂θ∂x) - ζ * (∂β∂x∂wₖ₊₁ + ∂θ∂x∂wₖ₊₁)) + μ / (2*Δx))
+    ∂μ∂wₖ   = 2 * (Δx * (∂ζ∂x∂wₖ   * (β + θ) + ∂ζ∂x * (∂β∂wₖ   + ∂θ∂wₖ)   - ∂ζ∂wₖ   * (∂β∂x + ∂θ∂x) - ζ * (∂β∂x∂wₖ   + ∂θ∂x∂wₖ)) - μ / (2*Δx))
     ∂μ∂hₖ   = 2 * (Δx * (∂ζ∂x∂hₖ   * (β + θ) + ∂ζ∂x * (∂β∂hₖ   + ∂θ∂hₖ)   - ∂ζ∂hₖ   * (∂β∂x + ∂θ∂x) - ζ * (∂β∂x∂hₖ   + ∂θ∂x∂hₖ)))
     ∂μ∂hₖ₊₁ = 2 * (Δx * (∂ζ∂x∂hₖ₊₁ * (β + θ) + ∂ζ∂x * (∂β∂hₖ₊₁ + ∂θ∂hₖ₊₁) - ∂ζ∂hₖ₊₁ * (∂β∂x + ∂θ∂x) - ζ * (∂β∂x∂hₖ₊₁ + ∂θ∂x∂hₖ₊₁)))
-    ∂μ∂dₖ   = 2 * (Δx * (∂ζ∂x∂dₖ   * (β + θ) + ∂ζ∂x * (∂β∂dₖ   + ∂θ∂dₖ)   - ∂ζ∂dₖ   * (∂β∂x + ∂θ∂x) - ζ * (∂β∂x∂dₖ   + ∂θ∂x∂dₖ)))
-    ∂μ∂dₖ₊₁ = 2 * (Δx * (∂ζ∂x∂dₖ₊₁ * (β + θ) + ∂ζ∂x * (∂β∂dₖ₊₁ + ∂θ∂dₖ₊₁) - ∂ζ∂dₖ₊₁ * (∂β∂x + ∂θ∂x) - ζ * (∂β∂x∂dₖ₊₁ + ∂θ∂x∂dₖ₊₁)))
+    ∂μ∂dₖ   = 2 * (Δx * (∂ζ∂x * (∂β∂dₖ   + ∂θ∂dₖ)   - ∂ζ∂dₖ   * (∂β∂x + ∂θ∂x) - ζ * (∂θ∂x∂dₖ   - 1)))
+    ∂μ∂dₖ₊₁ = 2 * (Δx * (∂ζ∂x * (∂β∂dₖ₊₁ + ∂θ∂dₖ₊₁) - ∂ζ∂dₖ₊₁ * (∂β∂x + ∂θ∂x) - ζ * (∂θ∂x∂dₖ₊₁ - 1)))
 
+    ∂y∂wₖ   = 2 * (Δx * ((ζ * (∂β∂wₖ   + ∂θ∂wₖ  )) / (β + θ)^2 - ∂ζ∂wₖ   / (β + θ)) + ζ/(β + θ)) + 1
+    ∂y∂wₖ₊₁ = -∂y∂wₖ + 1
+    ∂y∂hₖ   = 2 * (Δx * ((ζ * (∂β∂hₖ   + ∂θ∂hₖ  )) / (β + θ)^2 - ∂ζ∂hₖ   / (β + θ)))
+    ∂y∂hₖ₊₁ = 2 * (Δx * ((ζ * (∂β∂hₖ₊₁ + ∂θ∂hₖ₊₁)) / (β + θ)^2 - ∂ζ∂hₖ₊₁ / (β + θ)))
+    ∂y∂dₖ   = 2 *  Δx *  (ζ * (∂β∂dₖ   + ∂θ∂dₖ  )) / (β + θ)^2 
+    ∂y∂dₖ₊₁ = 2 *  Δx *  (ζ * (∂β∂dₖ₊₁ + ∂θ∂dₖ₊₁)) / (β + θ)^2 
 
-    ∂y∂wₖ   = 2 * (((ζ * (∂β∂wₖ   + ∂θ∂wₖ  )) / (β + θ)^2) * Δx - ∂ζ∂wₖ   * (β + θ) - ζ/(β + θ)) + 1
-    ∂y∂wₖ₊₁ = 2 * (((ζ * (∂β∂wₖ₊₁ + ∂θ∂wₖ₊₁)) / (β + θ)^2) * Δx - ∂ζ∂wₖ₊₁ * (β + θ) + ζ/(β + θ))
-    ∂y∂hₖ   = 2 * (((ζ * (∂β∂hₖ   + ∂θ∂hₖ  )) / (β + θ)^2) * Δx - ∂ζ∂hₖ   * (β + θ))
-    ∂y∂hₖ₊₁ = 2 * (((ζ * (∂β∂hₖ₊₁ + ∂θ∂hₖ₊₁)) / (β + θ)^2) * Δx - ∂ζ∂hₖ₊₁ * (β + θ))
-    ∂y∂dₖ   = 2 *  ((ζ * (∂β∂dₖ   + ∂θ∂dₖ  )) / (β + θ)^2) * Δx
-    ∂y∂dₖ₊₁ = 2 *  ((ζ * (∂β∂dₖ₊₁ + ∂θ∂dₖ₊₁)) / (β + θ)^2) * Δx
-
-    # TODO: check the derivaties with respect to the abs(), make case distinction?
-    ∂LogJac∂wₖ   = (1 / abs(μ)) * ∂μ∂wₖ   - (2 / abs(β + θ)) * (∂β∂wₖ   + ∂θ∂wₖ)
-    ∂LogJac∂wₖ₊₁ = (1 / abs(μ)) * ∂μ∂wₖ₊₁ - (2 / abs(β + θ)) * (∂β∂wₖ₊₁ + ∂θ∂wₖ₊₁)
-    ∂LogJac∂hₖ   = (1 / abs(μ)) * ∂μ∂hₖ   - (2 / abs(β + θ)) * (∂β∂hₖ   + ∂θ∂hₖ)
-    ∂LogJac∂hₖ₊₁ = (1 / abs(μ)) * ∂μ∂hₖ₊₁ - (2 / abs(β + θ)) * (∂β∂hₖ₊₁ + ∂θ∂hₖ₊₁)
-    ∂LogJac∂dₖ   = (1 / abs(μ)) * ∂μ∂dₖ   - (2 / abs(β + θ)) * (∂β∂dₖ   + ∂θ∂dₖ)
-    ∂LogJac∂dₖ₊₁ = (1 / abs(μ)) * ∂μ∂dₖ₊₁ - (2 / abs(β + θ)) * (∂β∂dₖ₊₁ + ∂θ∂dₖ₊₁)
+    ∂LogJac∂wₖ   = (1 / μ) * ∂μ∂wₖ   - (2 / (β + θ)) * (∂β∂wₖ   + ∂θ∂wₖ)
+    ∂LogJac∂wₖ₊₁ = -∂LogJac∂wₖ
+    ∂LogJac∂hₖ   = (1 / μ) * ∂μ∂hₖ   - (2 / (β + θ)) * (∂β∂hₖ   + ∂θ∂hₖ)
+    ∂LogJac∂hₖ₊₁ = (1 / μ) * ∂μ∂hₖ₊₁ - (2 / (β + θ)) * (∂β∂hₖ₊₁ + ∂θ∂hₖ₊₁)
+    ∂LogJac∂dₖ   = (1 / μ) * ∂μ∂dₖ   - (2 / (β + θ)) * (∂β∂dₖ   + ∂θ∂dₖ)
+    ∂LogJac∂dₖ₊₁ = (1 / μ) * ∂μ∂dₖ₊₁ - (2 / (β + θ)) * (∂β∂dₖ₊₁ + ∂θ∂dₖ₊₁)
 
     # Transformed output
     y =  wₖ - 2 * (ζ / (β + θ)) * Δx
-    
+
     # LogJacobian
     LogJac = log(abs(μ)) - 2*log(abs(β + θ))
 
@@ -402,3 +407,7 @@ function eval_backward_rqs_params_with_grad(
 
     return y, LogJac, ∂y∂w, ∂y∂h, ∂y∂d, ∂LogJac∂w, ∂LogJac∂h, ∂LogJac∂d
 end
+
+# For debugging 
+export eval_backward_rqs_params_with_grad
+export eval_forward_rqs_params_with_grad
